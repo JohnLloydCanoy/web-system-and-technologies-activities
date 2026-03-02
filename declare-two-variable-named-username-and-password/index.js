2ba10cf8dc0dfd0c99f3efc5d75583fe8bddc723
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-02 14:55:26 +0800

    Making a index.html

--- declare-two-variable-named-username-and-password/index.js
+++ declare-two-variable-named-username-and-password/index.js
@@ -5,7 +5,7 @@ const inputUsername = prompt("Enter your username:");
 const inputPassword = prompt("Enter your password:");
 
 if (inputUsername === adminUsername && inputPassword === adminPassword) {
-  alert("Login successful!");
+    alert("Login successful!");
 } else {
-  alert("Invalid username or password. Login failed.");
+    alert("Invalid username or password. Login failed.");
 }
